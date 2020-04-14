ac8d402d96be567174bef78914aeab7429b88a10
RNV Sundeep <routulasundeep@msitprogram.net>
2020-04-14 13:46:04 +0530

    added methods

--- LRU.py
+++ LRU.py
@@ -1,7 +1,19 @@
 from queue import Queue
+from collections import deque
 
 class LRU:
-    def __init__(self, size):
+    def __init__(self):
         # initialize a queue when LRU is called
-        self.queue = Queue(maxsize = 5)
-        
+        # q = Queue.Queue(maxsize=0)
+        # self.queue = Queue(maxsize=0)
+        self.queue = deque(maxlen = 5)
+    
+    def put(self, item):
+        # self.queue.append(item)
+        self.queue.append(item)
+
+    def get(self):
+        return self.queue.popleft()
+
+    def get_cache(self):
+        return self.queue
